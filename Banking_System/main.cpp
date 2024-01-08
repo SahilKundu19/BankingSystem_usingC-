a53eceffbaf2d39dcdc63f6f545d84dda63a03e3
Sahil <kundubabita991@gmail.com>
2024-01-08 13:21:34 +0530

    initial setting for the project

--- Banking_System/main.cpp
+++ Banking_System/main.cpp
@@ -1,14 +1,5 @@
-//
-//  main.cpp
-//  Banking_System
-//
-//  Created by Sahil Kundu on 08/01/24.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    
 }
